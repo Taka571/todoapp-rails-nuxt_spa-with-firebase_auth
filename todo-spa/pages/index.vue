5251bfe2cd044e20af00474e932d263aca49662a
Takahiro <tk.iyei@gmail.com>
2020-02-22 10:35:08 +0900

    add user.name index.vue from store

--- todo-spa/pages/index.vue
+++ todo-spa/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+  <div v-if="user">
+    <p>{{user.name}}</p>
     <AddTodo @submit="addTodo" />
     <TodoList :todos="todos" />
   </div>
@@ -13,6 +14,11 @@ export default {
   components: {
     AddTodo,
     TodoList
+  },
+  computed: {
+    user() {
+      return this.$store.state.currentUser; 
+    }
   },
   data() {
     return {
